d17cc7c421b7a0bc822315920a3049ff2ec03378
vashchenkosergey96 <vashchenkosergey96@icloud.com>
2019-05-24 12:50:15 +0300

    final commit

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 const app = new PIXI.Application();
 document.body.appendChild(app.view);
-
-// load spine data
 app.loader
     .add('goblins', './assets/goblin.json')
     .load(onAssetsLoaded);
 
 app.stage.interactive = true;
 app.stage.buttonMode = true;
+alert('kill him');
 
 function onAssetsLoaded(loader, res) {
   const goblin = new PIXI.spine.Spine(res.goblins.spineData);
-
-  // set current skin
   goblin.skeleton.setSkinByName('goblin');
   goblin.skeleton.setSlotsToSetupPose();
 
-  // set the position
   goblin.x = 400;
   goblin.y = 600;
 
   goblin.scale.set(1.5);
 
-  // play animation
   goblin.state.setAnimation(0, 'walk', true);
 
   app.stage.addChild(goblin);
 
   app.stage.on('pointertap', () => {
-    // change current skin
-    const currentSkinName = goblin.skeleton.skin.name;
-    const newSkinName = (currentSkinName === 'goblin' ? 'goblingirl' : 'goblin');
-    goblin.skeleton.setSkinByName(newSkinName);
-    goblin.skeleton.setSlotsToSetupPose();
+    var audio = document.getElementById("audio");
+    audio.play();
+    setTimeout("  alert('Redirect to the AppStore')", 1500);
   });
 }
